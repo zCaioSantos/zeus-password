9649a6b3e64f76883d9124955809fe3736a70c1b
zCaioSantos <caio.fernandes72@gmail.com>
2022-07-06 23:39:19 -0300

    PUT: Background image FIXED: Home Page CSS

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -7,16 +7,16 @@ interface IInputProps {
     secondIcon?: ReactNode;
 }
 
-const Input = ( { type, placeholder, icon, secondIcon }:IInputProps ) => {
+const Input = ({ type, placeholder, icon, secondIcon }: IInputProps) => {
     return (
-        <div className="flex items-center gap-[10px] border-[0.5px] p-5 rounded-2xl max-w-[600px] border-white">
-            { icon }
+        <div className="flex items-center gap-2 border-[0.5px] py-3 px-5 h-14 rounded-xl max-w-[592px] border-gray-500 transition-all hover:border-white focus-within:border-white">
+            {icon}
             <input
-                className="bg-transparent border-none outline-none flex-1 text-gray-900 h-full placeholder:text-base text-gray-900"
+                className="flex-1 text-gray-900 bg-transparent border-none outline-none text-base"
                 type={type}
                 placeholder={placeholder}
             />
-            { secondIcon }
+            {secondIcon}
         </div>
     );
 };
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import { Logo } from './Logo'
 
 const Navbar = () => {
     return (
-        <header className="w-full py-5 flex items-center justify-center bg-black">
+        <header className="w-full py-5 flex items-center justify-center bg-black bg-opacity-20 backdrop-blur-lg">
             <Logo />
         </header>
     )
