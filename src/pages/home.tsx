c3d5c3a34dd6704554669f21f4b8d7227b75e4aa
Caio Santos <caio.fernandes72@gmail.com>
2022-07-07 16:00:44 -0300

    FIXED: Page Home

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -5,7 +5,7 @@ const Home = () => {
     return (
         <div className="flex flex-col min-h-screen">
             <Navbar />
-            <section className="flex-1 w-full max-w-[1440px] mx-auto flex flex-col justify-center gap-6 animate-fadeIn">
+            <section className="flex-1 w-full max-w-[1440px] sm:px-10 mx-auto flex flex-col justify-center gap-6 animate-fadeIn">
                 <div className="flex flex-col items-start gap-2">
                     <h1 className="font-bold text-5xl bg-clip-text text-transparent bg-gradient-to-r from-cyan-500 to-pink-500">
                         Generate a secure password
