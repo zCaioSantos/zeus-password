186dbdeb8b5cb8b579a0a2279341433bea7ff76b
zCaioSantos <caio.fernandes72@gmail.com>
2022-07-06 23:33:42 -0300

    test

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,42 +1,42 @@
 import Navbar from "../components/Navbar";
+
 import Input from "../components/Input";
 
 import { FaUserAlt, FaLock } from "react-icons/fa";
 import { FiCopy } from "react-icons/fi";
 
-import Footer from "../components/Footer";
+
 
 const Home = () => {
     return (
         <div className="flex flex-col items-center min-h-screen">
-            <Navbar />
-            <section className="flex-1 flex flex-col gap-20 w-full bg-home bg-cover p-[100px]">
 
-                <div className="flex flex-col items-start gap-[10px]">
+            <Navbar />
+
+            <section className="flex-1 w-full px-28 flex flex-col justify-center gap-6">
+                <div className="flex flex-col items-start gap-2">
                     <h1 className="font-bold text-5xl bg-clip-text text-transparent bg-gradient-to-r from-cyan-500 to-pink-500">
                         Generate a secure password
                     </h1>
-                    <p className="font-normal text-base text-white tracking-wide">
+                    <p className="font-normal text-base text-white tracking-wide leading-relaxed">
                         Use your online secure password generator to create a secure password
                     </p>
                 </div> 
 
-                <form className="flex flex-col gap-8">
+                <form className="flex flex-col gap-2">
                     <Input placeholder="Name or E-mail" type="text" icon={<FaUserAlt color="bbbbbb" size={13} />} />
                     <Input
                         placeholder="Generated password"
                         type="text"
                         icon={<FaLock color="bbbbbb" size={13} />}
-                        secondIcon={<FiCopy color="bbbbbb" className="cursor-pointer" size="20px" />}
+                        secondIcon={<FiCopy color="bbbbbb" className="cursor-pointer hover:stroke-white" size="20px" title="Click to copy" />}
                     />
-                    <button className="bg-white text-black font-bold text-base rounded-2xl max-w-[300px] p-5">
+
+                    <button className="max-w-[275px] h-14 p-3 rounded-xl text-base mt-4 font-bold text-black bg-white transition-all hover:bg-transparent hover:border-[0.5px] hover:border-white hover:text-white">
                         To generate
                     </button>
                 </form>
-
             </section>
-
-            <Footer />
         </div>
     );
 };
