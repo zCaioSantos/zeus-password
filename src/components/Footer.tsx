d9a5899229b38e244960851350a0799459037f02
zCaioSantos <caio.fernandes72@gmail.com>
2022-07-06 23:33:21 -0300

    REMOVE: Footer

--- src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-    return (
-        <footer className="w-full py-5 flex items-center justify-center bg-black">
-            <strong className="text-gray-500 font-normal">By zCaioSantos</strong>
-        </footer>
-    )
-}
-
-export default Footer;
